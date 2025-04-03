64db2f29ef56cc6fa37386808483a82bd8468542
danaeswart <241022@virtualwindow.co.za>
2025-04-03 09:23:39 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import Home from './components/Home';
 import About from './components/About';
 import Services from './components/Services';
 import Contact from './components/Contact';
-import { Router, Routes } from 'react-router-dom';
+import { Router, Routes} from 'react-router-dom';
 import Footer from './components/Footer';
+
 
 function App() {
   return (
