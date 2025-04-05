87c36b2181070bf10de57db5b583ea773c54ee03
Kai-Barker <241065@virtualwindow.co.za>
2025-04-05 15:41:04 +0200

    About us page, added cards that take in data from anywhere

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,16 @@
 import Mission from "./Mission";
 import ListItemsAbout from "./ListItemsAbout.js";
+import Card from "./CardMeet.js";
+import MeetTheTeam from "./MeetTheTeam.js";
 
 
 function About () {
+    
     return (
         <div>
             <Mission />
             <ListItemsAbout />
+            <MeetTheTeam />
         </div>
     )
 }
--- src/components/ListItemsAbout.js
+++ src/components/ListItemsAbout.js
@@ -5,7 +5,7 @@ function ListItems() {
   return (
     <div>
       <Container style={{width:'60%'}}>
-        <Row style={{alignItems:'center', margin:'0', marginTop:'25px'}}><h1 style={{fontWeight:'bold'}}>Why Choose Us</h1></Row>
+        <Row style={{alignItems:'center', margin:'0', marginTop:'25px'}}><h1 style={{fontWeight:'bold'}}>Why Choose Us?</h1></Row>
         <Row style={{justifyContent:'left', textAlign:'left', alignItems:'center', margin:'0', marginTop:'25px'}}>
           <div style={{width: '50px',height: '50px',backgroundColor: '#5160A5',borderRadius: '50%',}}></div>
           <p style={{margin:'0', fontSize:'24px', color:'black', fontWeight:'bold', width:'auto'}}>Professional and skilled VAs ready to assist you</p>
