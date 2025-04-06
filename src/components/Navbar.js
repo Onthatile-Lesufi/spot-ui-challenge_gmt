edbd67a9090eaf0cb5d2bfdce14067fedff7a8ac
Onthatile-Lesufi <231262@virtualwindow.co.za>
2025-04-06 13:20:26 +0200

    Update Service Carousel

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
@@ -6,24 +6,47 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import './css/Navbar.css';
 
 function NavbarComponent() {
-  return (
-    <Navbar expand="lg" className="navbar-container">
-      <Container>
-        <Navbar.Brand href="#home">Great Minds Technologies</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <div className="link-container">
-                <Link className="navbar-link" to="/">Home</Link>
-                <Link className="navbar-link" to="/about">About Us</Link>
-                <Link className="navbar-link" to="/services">Our Services</Link>
-                <Link className="navbar-link" to="/contact-us">Contact Us</Link>
-            </div>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+    const navHeading = [{
+        value: '/',
+        label:"Home"
+    },{
+        value: '/about',
+        label:"About Us"
+    },{
+        value: '/services',
+        label:"Our Services"
+    },{
+        value: '/contact-us',
+        label:"Contact Us"
+    }]
+
+    return (
+      <Navbar expand="lg" className="navbar-container">
+        <Container>
+            <Link className="navbar-link" to="/">
+                <Navbar.Brand>Great Minds Technologies</Navbar.Brand>
+            </Link>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <div className="link-container">
+                    {navHeading.map((_heading) => (
+                        <NavLink 
+                        to={_heading.value}
+                        className="navbar-link"
+                        style={({ isActive }) => ({
+                            fontWeight: isActive ? '600' : 'normal'
+                        })}
+                        >
+                            {_heading.label}
+                        </NavLink>
+                    ))}
+                </div>
+              </Nav>
+            </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
 }
 
 export default NavbarComponent;
--- src/components/Services.js
+++ src/components/Services.js
@@ -15,19 +15,54 @@ function Services () {
 
             <Carousel className='services-carousel'>
               <Carousel.Item className='carousel-item'>
+                <Carousel.Caption>
+                  <h3>Administrative Support</h3>
+                  <p>Calendar management, travel planning, document creation, and data entry.</p>
+                  <Link to='/contact-us'>
+                    <Button id='consultation-button'>Contact Us Now</Button>
+                  </Link>
+                </Carousel.Caption>
                 <img src={admin}/>
               </Carousel.Item>
               <Carousel.Item className='carousel-item'>
                 <img src={support}/>
+                <Carousel.Caption>
+                  <h3>Customer Service Support</h3>
+                  <p>Handling customer inquiries, support ticket management, phone call assistance, and live chat support.</p>
+                  <Link to='/contact-us'>
+                    <Button id='consultation-button'>Contact Us Now</Button>
+                  </Link>
+                </Carousel.Caption>
               </Carousel.Item>
               <Carousel.Item className='carousel-item'>
                 <img src={socials}/>
+                <Carousel.Caption>
+                  <h3>Social Media Management</h3>
+                  <p>Creating and scheduling posts, managing engagement, and developing growth strategies for platforms like Instagram, LinkedIn, and Facebook.</p>
+                  <Link to='/contact-us'>
+                    <Button id='consultation-button'>Contact Us Now</Button>
+                  </Link>
+                </Carousel.Caption>
               </Carousel.Item>
               <Carousel.Item className='carousel-item'>
                 <img src={management}/>
+                <Carousel.Caption>
+                  <h3>Project Management</h3>
+                  <p>Task tracking, deadline management, and team coordination through tools like Asana and Trello.</p>
+                  <Link to='/contact-us'>
+                    <Button id='consultation-button'>Contact Us Now</Button>
+                  </Link>
+                </Carousel.Caption>
               </Carousel.Item>
               <Carousel.Item className='carousel-item'>
                 <img src={specialSupport}/>
+                <Carousel.Caption>
+                  <h3>Specialized Support</h3>
+                  <p>Bookkeeping, CRM management, event planning, and market research.</p>
+                  <Link to='/contact-us'>
+                    <Button id='consultation-button'>Contact Us Now</Button>
+                  </Link>
+                </Carousel.Caption>
               </Carousel.Item>
             </Carousel> 
 
